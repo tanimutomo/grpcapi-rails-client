5d9170624a9d45e86a56b15fdee2f1acbf909dc6
tanimutomo <tanimutomo@gmail.com>
2020-11-06 02:18:39 +0900

    fix: services/article_api

--- app/services/article_api.rb
+++ app/services/article_api.rb
@@ -4,7 +4,4 @@ require 'proto/article_services_pb'
 
 class ArticleApi
   HOST = 'localhost:50051'
-  def initialize
-    @service = Proto::Article::Article::Stub.new('localhost:50051', :this_channel_is_insecure)
-  end
 end
